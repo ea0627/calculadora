f9b1b0976fcefe52c8ab309e3a652daf456344a7
Eduardo Andres Amaya Muñoz <eduardo.amaya627@gmail.com>
2024-07-05 18:32:44 -0500

    proyecto calculadora nivel 3

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const numero1 = parseInt(prompt("Ingresa el primer numero"));
 const numero2 = parseInt(prompt("Ingresa el segundo numero"));
-
-
+const numero3 = parseInt(prompt("Ingresa el tercer numero"));
 
 const suma = numero1 + numero2;
 const resta = numero1 - numero2;
 const multi = numero1 * numero2;
 const div = numero1 / numero2;
-
+const prom = 100;
+ 
 console.log("El numero 1 es: ", numero1);
 console.log("El numero 2 es: ", numero2); 
 console.log("Calculadora Nivel 1");
@@ -46,4 +46,47 @@ console.log("El resultado de la suma de ", numero1, " y ", numero2, " Utilizando
 console.log("El resultado de la resta de ", numero1, " y ", numero2, " Utilizando funciones es:", resuladoresta);
 console.log("El resultado de la multiplicacion de ", numero1, " y ", numero2, " Utilizando funciones es:", resultadomultiplicar);
 console.log("El resultado de la division de ", numero1, " y ", numero2, " Utilizando funciones es:", resultadodividir);
+
+
+// Calculadora Nivel III
+console.log("Calculadora Nivel 3");
+
+const numero4 = parseInt(prompt("Que porcentaje desea calcular?"));
+const numero5 = parseInt(prompt("De que numero?"));
+const numero6 = parseInt(prompt("Que porcentaje desea generar?"));
+const numero7 = parseInt(prompt("De que numero?"));
+
+
+function cuadradoDeUnNumero(a,b) {
+    const x= multiplicar(numero1,numero1);
+    return x;
+}
+
+
+function promedioDeTresNumeros(a, b, c) {
+    const suma = sumar(numero1, numero2, numero3);
+    return suma / 3;
+}
+
+function calcularPorcentaje(a, b) {
+    const producto = multiplicar(numero4, numero5);
+    return producto / 100;
+}
+
+function GeneradorDePorcentaje(a, b) {
+    const producto = multiplicar(numero6,100);
+    return producto / numero7;
+}
+
+
+const promedio = promedioDeTresNumeros(numero1, numero2, numero3)
+const cuadrado = cuadradoDeUnNumero(numero1)
+const porcentaje = calcularPorcentaje(numero4, numero5)
+const genporcentaje = GeneradorDePorcentaje(numero6, numero7);
+
+
+console.log("El resultado del cuadrado de ", numero1, " es ", cuadrado);
+console.log("El resultado del promedio de ", numero1, ",", numero2, " y ", numero3, " es: ", promedio);
+console.log("El ", numero4, "% de ", numero5, " es: ", porcentaje);
+console.log(genporcentaje, "corresponde al", numero6, "% de", numero7);
 
