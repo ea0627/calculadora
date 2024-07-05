4d21e607a06c435b9fcb3ae5768a3be241c64dd6
Eduardo Andres Amaya Muñoz <eduardo.amaya627@gmail.com>
2024-07-04 19:09:38 -0500

    Proyecto calculadora 3 julio

---
+++ index.js
@@ -0,0 +1,14 @@
+const numero1 = parseInt(prompt("Ingresa el primer numero"));
+const numero2 = parseInt(prompt("Ingresa el segundo numero"));
+
+const suma = numero1 + numero2;
+const resta = numero1 - numero2;
+const multi = numero1 * numero2;
+const div = numero1 / numero2;
+
+console.log("El numero 1 es: ", numero1);
+console.log("El numero 2 es: ", numero2); 
+console.log("La suma de los dos numeros es: ", suma);
+console.log("La resta de los dos numeros es: ", resta);
+console.log("La multiplicacin de los dos numeros es: ", multi);
+console.log("la division de los dos numeros es: ", div);
